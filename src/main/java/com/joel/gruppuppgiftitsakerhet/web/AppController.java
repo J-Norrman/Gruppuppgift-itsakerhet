c0c68c57f57438946f6897c2ad429977bb2c4c7e
Janainaacm <janaina.maneiro@gmail.com>
2024-06-14 12:32:03 +0200

    error and unauthorized pages

--- src/main/java/com/joel/gruppuppgiftitsakerhet/web/AppController.java
+++ src/main/java/com/joel/gruppuppgiftitsakerhet/web/AppController.java
@@ -8,6 +8,8 @@ import jakarta.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.ProviderNotFoundException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
@@ -17,7 +19,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -157,4 +161,12 @@ public class AppController {
         }
         return "redirect:/users";
     }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    @GetMapping("/unauthorized")
+    public String handleAccessDeniedException() {
+        return "unauthorized";
+    }
+
+
 }
